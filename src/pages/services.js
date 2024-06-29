ed56a935486604646e3d94ddf6ce7d8d401b5b0a
SALMAN KHAN <salmankhan.sk302123@gmail.com>
2024-06-29 22:33:02 +0500

    29-6 class

--- src/pages/services.js
+++ src/pages/services.js
@@ -1,8 +1,17 @@
 import React from "react";
+import {Fade} from "react-awesome-reveal"
+import ReactVisibilitySensor from "react-visibility-sensor";
 export default function Services(){
     return(
         <div>
-            <h1>Services page</h1>
+            <div className="flex justify-center items-center h-screen">
+                <ReactVisibilitySensor>
+                    <Fade direction="up">
+                        <h1 className="text-5xl  font-extrabold">Our Exlusive <span className="text-blue-500">ITSolution</span><br/> Services</h1>
+                    </Fade>
+                </ReactVisibilitySensor>
+            </div>
+
         </div>
     )
 }
