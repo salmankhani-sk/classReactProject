f9400c26672a435139fb498ceb00d3e82979ef48
SALMAN KHAN <salmankhan.sk302123@gmail.com>
2024-07-07 23:30:57 +0500

    class

--- src/pages/contactus.js
+++ src/pages/contactus.js
@@ -1,8 +1,38 @@
 import React from "react";
+import { IoLocationOutline } from "react-icons/io5";
+import { FaPhoneAlt } from "react-icons/fa";
+import { IoLogoInstagram } from "react-icons/io5";
 export default function Contactus(){
     return(
         <div>
-            <h1>Contact us page</h1>
+            <div className="bg-gradient-to-b from-blue-100 to-pink-200 flex flex-col items-center justify-center">
+                <div>
+                    <div>
+                        <div className="flex items-center justify-center mx-8">
+                            <div>
+                                <a href="https://www.google.com/maps/place//@33.7745676,73.2018521,14.26z?entry=ttu" target='_blank' rel="noreferrer">
+                                <IoLocationOutline />
+                                <h2>Address: <span>Islamabad Pakistan</span></h2>
+                                </a>
+
+                            </div>
+                            <div><a href="https://wa.me/923175566089" target='_blank' rel="noreferrer" >
+                            <FaPhoneAlt/>
+                            <h2>+923175566089</h2>
+                            </a>
+
+                            </div>  
+                            <div>
+                                <a href="https://www.instagram.com/salmankhani_sk/" target="_blank" rel="noreferrer" >
+                                <IoLogoInstagram/>
+                                <h2>Instagram: <span>@salmankhani_sk</span></h2>
+
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/pages/singin.js
+++ src/pages/singin.js
@@ -1,8 +1,18 @@
-import React from "react";
-export default function Signin(){
-    return(
-        <div>
-            <h1>sign in page</h1>
+import React from 'react';
+
+function SignIn() {
+    return (
+        <div className="flex items-center justify-center h-screen bg-gray-200">
+            <div className="bg-white p-8 rounded shadow-md w-80">
+                <h2 className="text-2xl font-bold mb-8 text-center">Sign In</h2>
+                <form>
+                    <input className="mb-4 w-full px-3 py-2 border border-gray-300 rounded-md" type="email" placeholder="Email" required />
+                    <input className="mb-4 w-full px-3 py-2 border border-gray-300 rounded-md" type="password" placeholder="Password" required />
+                    <button className="w-full py-2 px-4 bg-blue-600 text-white rounded-md" type="submit">Sign In</button>
+                </form>
+            </div>
         </div>
-    )
+    );
 }
+
+export default SignIn;
